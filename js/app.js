470bc5336a294a396b98ba81bc85d5df10311a5b
Koraboevvv <bexruzkarabaev@gmail.com>
2025-04-29 12:03:53 +0500

    z

---
+++ js/app.js
@@ -0,0 +1,86 @@
+// M-1
+
+
+function kop (n){
+    return (n ** 2)
+}
+console.log(kop(5));
+console.log(kop(9));
+
+// M-2
+
+function name(ism) {
+    return `Salom ${ism}`
+}
+console.log(name("Bexruz"));
+console.log(name("Bobur ustozim"));
+
+// M-3
+
+function A1(n) {
+    if (n >= 0) {
+        return "Musbat"
+    } else{
+        return "Manfiy"
+    }
+}
+console.log(A1(-5));
+console.log(A1(5));
+
+// M-4
+
+function A2(n) {
+    if (n % 2 === 0) { 
+        return 'juft'; 
+    } else {
+        return 'toq'; 
+    }
+}
+
+console.log(A2(4)); 
+console.log(A2(3)); 
+
+// m-5
+
+// function A3(n) {
+//     let a = +prompt();
+//     let b = +prompt(); 
+//     if ( a - b < 0) {
+//         return (b)
+//     } else {
+//         return (a)
+//     }
+// }
+// console.log(A3(4));
+// console.log(A3(7));
+
+// M-6
+
+function A6(n) {
+    for (let i = 1; i <= n; i++) {
+        console.log (i);
+        
+    }
+}
+console.log(A6(11));
+
+
+// M-7
+
+// function A7(n) {
+//     for (let i = 0; i < n ; i + n) {
+//         // const element = [i];
+//         console.log(i + n);
+        
+//     }
+// }
+// console.log(A7(7));
+
+function A7(n) {
+    let a = [1,2,3,4,5]
+    let b = 0
+    for (let b = 0; b < array.length; b++) {
+        const element = array[b];
+        
+    }
+}
